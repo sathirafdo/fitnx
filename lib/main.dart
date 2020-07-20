03665eb2b1f2249df0b1c3b6f91db14ec07515d4
sathirafdo <wsdfdo@gmail.com>
2020-07-20 13:47:03 +0530

    test add

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Fit-nx'),
+      home: MyHomePage(title: 'Fit-nx '),
     );
   }
 }
